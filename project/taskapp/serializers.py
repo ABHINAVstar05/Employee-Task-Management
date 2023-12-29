90e667a5948f13d10979a76c42203830b2009132
Abhinav <kumar.abhinav3125@gmail.com>
2023-12-30 00:54:08 +0530

    Implemented code to automatically populate the Lead and Subordinate tables when corresponding Employee is created.

--- project/taskapp/serializers.py
+++ project/taskapp/serializers.py
@@ -6,3 +6,21 @@ class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
         fields = '__all__'
+
+
+class LeadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lead
+        fields = '__all__'
+
+
+class SubordinateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subordinate
+        fields = '__all__'
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
--- project/taskapp/views.py
+++ project/taskapp/views.py
@@ -6,6 +6,18 @@ from taskapp.serializers import *
 
 # Create your views here.
 
+
+# Automatically populate the Lead and Subordinate tables when corresponding Employee is created.
+def fill_role(obj, data) :
+    if data['role'] == 'Lead' :
+        Lead.objects.create(employee=obj)
+    elif data['role'] == 'Subordinate' :
+        Subordinate.objects.create(employee=obj)
+    else :
+        raise ValueError("Role must be 'Lead' or 'Subordinate'")
+    
+
+
 @api_view(['GET', 'POST', 'DELETE'])
 def emp(request) :
     
@@ -19,6 +31,8 @@ def emp(request) :
         deserialized = EmployeeSerializer(data = data)
         if deserialized.is_valid() :
             deserialized.save()
+            obj = Employee.objects.get(email = data['email'])
+            fill_role(obj, data)
             return Response(deserialized.data)
         else :
             return Response(deserialized.errors)
